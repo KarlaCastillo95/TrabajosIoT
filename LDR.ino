e25a5393158a620971d0702a1f631c2e5dbed038
KarlaCastillo95 <karlisbb95@gmail.com>
2024-11-05 08:33:05 -0600

    Add files via upload

---
+++ LDR.ino
@@ -0,0 +1,30 @@
+int ldr=  A5;
+int valor;
+int led =13;
+int limite=100;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); // frecuencia
+  pinMode(ldr,INPUT); // para poder recibir valoes 
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int valor = analogRead(ldr); //obtenr los valores de respuesta y guardarlos en la variavle valor  
+  Serial.println(valor); // imprimir los datos que se guardoaron el la variavle de  valor
+  delay(1000);
+
+  if(valor <=limite){
+    digitalWrite(led, HIGH);
+    delay(1000);
+   
+  }else{
+    digitalWrite(led, LOW);
+    
+  }
+  
+
+}
---
+++ Serie.ino
@@ -0,0 +1,70 @@
+int led1=13;
+int led2=12;
+int led3=11;
+int led4=10;
+int led5=9;
+int led6=8;
+int del = 500;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode (led1, OUTPUT);
+  pinMode (led2, OUTPUT);
+  pinMode (led3, OUTPUT);
+  pinMode (led4, OUTPUT);
+  pinMode (led5, OUTPUT);
+  pinMode (led6, OUTPUT);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly
+  for(int i=0; i<4; i++ ){
+
+     digitalWrite(led1, HIGH);
+     delay(del);
+     digitalWrite(led1, LOW);
+
+     digitalWrite(led2, HIGH);
+     delay(del);
+     digitalWrite(led2, LOW);
+
+     digitalWrite(led3, HIGH);
+     delay(del);
+     digitalWrite(led3, LOW);
+
+     digitalWrite(led4, HIGH);
+     delay(del);
+     digitalWrite(led4, LOW);
+
+     digitalWrite(led5, HIGH);
+     delay(del);
+     digitalWrite(led5, LOW);
+
+     digitalWrite(led6, HIGH);
+     delay(del);
+     digitalWrite(led6, LOW);
+
+     digitalWrite(led5, HIGH);
+     delay(del);
+     digitalWrite(led5, LOW);
+
+     digitalWrite(led4, HIGH);
+     delay(del);
+     digitalWrite(led4, LOW);
+
+     digitalWrite(led3, HIGH);
+     delay(del);
+     digitalWrite(led3, LOW);
+
+     digitalWrite(led2, HIGH);
+     delay(del);
+     digitalWrite(led2, LOW);
+
+     digitalWrite(led1, HIGH);
+     delay(del);
+     digitalWrite(led1, LOW);
+  }
+
+}
